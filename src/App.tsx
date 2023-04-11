bf38bdb8d0ae016d9f5406efaab9ce1e2c04703d
Clearenough <smogida1337@gmail.com>
2023-04-11 17:05:51 +0300

    feet: add pages and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Info from './pages/Info/Info';
+import Main from './pages/Main/Main';
+import NotFound from './pages/NotFound/NotFound';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="wrapper">
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/info/:id" element={<Info />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to={'/404'} />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Info/Info.tsx
@@ -0,0 +1,5 @@
+function Info() {
+  return <>Info Page</>;
+}
+
+export default Info;
