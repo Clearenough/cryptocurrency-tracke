ee5f979214e41522b38e6514b29d96867f41985e
Clearenough <smogida1337@gmail.com>
2023-04-14 00:36:29 +0300

    feet: add top currency to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,22 @@
 import styles from './Header.module.scss';
 import shop from './../../assets/svg/briefcase.svg';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import ModalBriefcase from '../common/modals/modalBriefcase/ModalBriefcase';
+import { CurrencyContext } from '../../context/currencyContext';
+import { numberParser } from '../../utils/numberParser';
 
 function Header() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const { currencyInfo } = useContext(CurrencyContext);
+  const popularCurrency = currencyInfo.slice(0, 3);
 
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <ul className={styles.currencyList}>
-          <li>Bitcoin: 123$</li>
-          <li>Bitcoin: 123$</li>
-          <li>Bitcoin: 123$</li>
+          {popularCurrency.map((currency) => (
+            <li key={currency.id}>{`${currency.name}: ${numberParser(currency.priceUsd)}$`}</li>
+          ))}
         </ul>
         <div className={styles.briefcase}>
           <img
